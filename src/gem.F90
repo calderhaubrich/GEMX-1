863b4b19e338cae92d4fad4a4ee0f3359d1ba5d1
Zhichen Feng <zhichen.feng@colorado.edu>
2024-08-22 13:34:55 -0600

    Clean up manual

--- src/gem.F90
+++ src/gem.F90
@@ -81,6 +81,9 @@
 !        write(*,*)"before petsc init"
        
        PetscCallA(PetscInitialize(petsc_ierr))
+
+
+
        PetscCallA(KSPCreate(PETSC_COMM_WORLD,ksp,petsc_ierr))
        PetscCallA(DMDACreate2D(PETSC_COMM_WORLD, DM_BOUNDARY_NONE,DM_BOUNDARY_NONE, DMDA_STENCIL_STAR,imx+1,jmx+1,PETSC_DECIDE,PETSC_DECIDE,one,one, PETSC_NULL_INTEGER,PETSC_NULL_INTEGER, dm, petsc_ierr))
        PetscCallA(DMSetFromOptions(dm,petsc_ierr))

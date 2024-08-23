7661d52c149352107215deabb9973be4c227a90a
Stefan Tirkas <stefan.tirkas@colorado.edu>
2024-08-23 17:29:47 -0600

    minor script and makefile change

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,4 @@
 module restore
 module load nvhpc
-export PETSC_DIR=/global/cfs/cdirs/mp118/software/petsc_new
-export PETSC_ARCH=perlmutter-nvhpc
-export LD_LIBRARY_PATH=$PETSC_DIR/$PETSC_ARCH/lib:$LD_LIBRARY_PATH
+export PETSC_PATH=/global/cfs/cdirs/mp118/software/petsc_new
+export LD_LIBRARY_PATH=$PETSC_PATH/lib:$LD_LIBRARY_PATH
